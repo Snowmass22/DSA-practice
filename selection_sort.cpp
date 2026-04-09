0f3f365ff5a87d998a8cb9398eb75a05211d7edc
tejas <tejaskale406@gmail.com>
2026-04-09 19:53:53 +0530

    selection_Sort

---
+++ selection_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void selection_sort(int arr[],int n){
+
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+}
+int main(){
+    int n,arr[n];
+    cout<<"enter size of the array";
+    cin>>n;
+    cout<<"enter elements in the array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"sorted array:"<<endl;
+    selection_sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;
+
+}
